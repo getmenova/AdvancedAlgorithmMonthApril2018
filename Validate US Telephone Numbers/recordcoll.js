20249c99409480689c07ef9093d57b3d75100c3e
Michael A Nowiszewski <micski@gmail.com>
2018-05-28 09:42:32 +0000

    add 2

---
+++ Validate US Telephone Numbers/recordcoll.js
@@ -0,0 +1,16 @@
+/*global collection */ //for c9
+function updateRecords(id, prop, value) {
+  if (prop === "tracks" && value !== "") {
+   if(collection[id][prop]) {
+    collection[id][prop].push(value);
+   }
+   else {
+    collection[id][prop]=[value];
+   }
+  } else if (value !== "") {
+    collection[id][prop] = value;
+  } else {
+    delete collection[id][prop];
+  }
+  return collection;
+}
